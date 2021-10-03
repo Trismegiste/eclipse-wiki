751365890e6863ac1492f605f3d98d4a58657b0c
Trismegiste <flo@trismegiste.net>
2021-10-03 14:03:22 +0200

    cleaning & refactor

--- src/Controller/GmHelper.php
+++ src/Controller/GmHelper.php
@@ -26,10 +26,10 @@ class GmHelper extends AbstractController
         $form = $this->createForm(LoveLetter::class);
         $form->handleRequest($request);
         if ($form->isSubmitted() && $form->isValid()) {
-            return $this->render('front/loveletter.html.twig', $form->getData());
+            return $this->render('loveletter.html.twig', $form->getData());
         }
 
-        return $this->render('front/template_form.html.twig', ['form' => $form->createView()]);
+        return $this->render('form.html.twig', ['form' => $form->createView()]);
     }
 
 }
