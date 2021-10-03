34802d770f09539fd233be43d06229596ac36913
Trismegiste <flo@trismegiste.net>
2021-10-03 22:03:09 +0200

    caching bg

--- src/Repository/BackgroundProvider.php
+++ src/Repository/BackgroundProvider.php
@@ -18,31 +18,36 @@ class BackgroundProvider extends GenericProvider
 
     public function findOne(string $key)
     {
-        $content = $this->wiki->getPageByName($key);
-        $doc = new DOMDocument("1.0", "utf-8");
-        $doc->loadXML($content);
-        $xpath = new \DOMXpath($doc);
-        $obj = new Background();
+        $sanitizedKey = str_replace(['%', '-'], '_', $key);
 
-        // abilities, thanks to http://xpather.com/
-        $elements = $xpath->query("//span[@id='Avantages']/parent::h2/following-sibling::ul[1]/li[text()]");
-        foreach ($elements as $li) {
-            $obj->ability[] = trim($li->nodeValue);
-        }
+        return $this->cache->get('background_page_' . $sanitizedKey, function (ItemInterface $item) use ($key) {
+                $content = $this->wiki->getPageByName($key);
+                $doc = new DOMDocument("1.0", "utf-8");
+                $doc->loadXML($content);
 
-        // disabiilties
-        $elements = $xpath->query("//span[@id='D.C3.A9savantages']/parent::h2/following-sibling::ul[1]/li[text()]");
-        foreach ($elements as $li) {
-            $obj->disability[] = trim($li->nodeValue);
-        }
+                $xpath = new \DOMXpath($doc);
+                $obj = new Background();
 
-        // motivations
-        $elements = $xpath->query("//span[@id='Motivations_sugg.C3.A9r.C3.A9es']/parent::h2/following-sibling::ul[1]/li[text()]");
-        foreach ($elements as $li) {
-            $obj->motivation[] = trim($li->nodeValue);
-        }
+                // abilities, thanks to http://xpather.com/
+                $elements = $xpath->query("//span[@id='Avantages']/parent::h2/following-sibling::ul[1]/li[text()]");
+                foreach ($elements as $li) {
+                    $obj->ability[] = trim($li->nodeValue);
+                }
 
-        return $obj;
+                // disabiilties
+                $elements = $xpath->query("//span[@id='D.C3.A9savantages']/parent::h2/following-sibling::ul[1]/li[text()]");
+                foreach ($elements as $li) {
+                    $obj->disability[] = trim($li->nodeValue);
+                }
+
+                // motivations
+                $elements = $xpath->query("//span[@id='Motivations_sugg.C3.A9r.C3.A9es']/parent::h2/following-sibling::ul[1]/li[text()]");
+                foreach ($elements as $li) {
+                    $obj->motivation[] = trim($li->nodeValue);
+                }
+
+                return $obj;
+            });
     }
 
     public function getListing(): array
