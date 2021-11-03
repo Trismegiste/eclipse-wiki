8b3fc49788d8d66898fcaa78107a683597c1db78
Trismegiste <flo@trismegiste.net>
2021-11-04 00:57:36 +0100

    using datamapper in SkillType but should go to SawoTraitType

--- src/Form/Type/SkillType.php
+++ src/Form/Type/SkillType.php
@@ -7,21 +7,25 @@
 namespace App\Form\Type;
 
 use App\Entity\Skill;
+use App\Repository\SkillProvider;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\DataMapperInterface;
+use Symfony\Component\Form\Exception\UnexpectedTypeException;
 use Symfony\Component\Form\Extension\Core\Type\HiddenType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Form\FormInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Traversable;
 
 /**
  * Value for a Skill
  */
-class SkillType extends AbstractType
+class SkillType extends AbstractType implements DataMapperInterface
 {
 
     protected $repository;
 
-    public function __construct(\App\Repository\SkillProvider $repo)
+    public function __construct(SkillProvider $repo)
     {
         $this->repository = $repo;
     }
@@ -41,7 +45,31 @@ class SkillType extends AbstractType
 
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder->add('name', HiddenType::class);
+        $builder->add('name', HiddenType::class)
+            ->setDataMapper($this)
+        ;
+    }
+
+    public function mapDataToForms($viewData, Traversable $forms)
+    {
+        if (is_null($viewData)) {
+            return;
+        }
+
+        // invalid data type
+        if (!$viewData instanceof Skill) {
+            throw new UnexpectedTypeException($viewData, Skill::class);
+        }
+
+        $fields = iterator_to_array($forms);
+        $fields['name']->setData($viewData->getName());
+    }
+
+    public function mapFormsToData(Traversable $forms, &$viewData)
+    {
+        $fields = iterator_to_array($forms);
+        $viewData = $this->repository->findOne($fields['name']->getData());
+        $viewData->dice = $fields['roll']->getData();
     }
 
 }
