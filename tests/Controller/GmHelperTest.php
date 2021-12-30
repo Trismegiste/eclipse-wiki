39bbbebeff3ad6581c1ad67ac06f5ae66a96e5e1
Trismegiste <flo@trismegiste.net>
2021-12-30 20:37:19 +0100

    func test for love letter

--- tests/Controller/GmHelperTest.php
+++ tests/Controller/GmHelperTest.php
@@ -14,28 +14,6 @@ class GmHelperTest extends WebTestCase
         $this->assertResponseIsSuccessful();
     }
 
-    public function testLoveLetter(): void
-    {
-        $client = static::createClient();
-        $crawler = $client->request('GET', '/gm/loveletter');
-        $this->assertResponseIsSuccessful();
-        $form = $crawler->selectButton('love_letter_generate')->form();
-        $form->setValues(['love_letter' => [
-                "player" => "ABCD",
-                "context" => "456",
-                "drama" => "789",
-                "roll1" => ["trait" => "Agilité", "difficulty" => -1],
-                "roll2" => ["trait" => "Agilité", "difficulty" => -1],
-                "roll3" => ["trait" => "Agilité", "difficulty" => -1],
-                "choice1" => "1",
-                "choice2" => "2",
-                "choice3" => "3",
-                "choice4" => "4"
-        ]]);
-        $crawler = $client->submit($form);
-        $this->assertEquals('Cher ABCD,', $crawler->filterXPath("//p[1]")->text());
-    }
-
     public function testNameGenerate()
     {
         $client = static::createClient();
---
+++ tests/Controller/LoveletterCrudTest.php
@@ -0,0 +1,86 @@
+<?php
+
+/*
+ * eclipse-wiki
+ */
+
+namespace App\Tests\Controller;
+
+use App\Repository\VertexRepository;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class LoveletterCrudTest extends WebTestCase
+{
+
+    protected $client;
+
+    protected function setUp(): void
+    {
+        $this->client = static::createClient();
+    }
+
+    public function testClean()
+    {
+        $repo = static::getContainer()->get(VertexRepository::class);
+        $repo->delete(iterator_to_array($repo->search()));
+        $this->assertCount(0, iterator_to_array($repo->search()));
+    }
+
+    public function testCreate()
+    {
+        $crawler = $this->client->request('GET', '/loveletter/create');
+        $this->assertResponseIsSuccessful();
+        $form = $crawler->selectButton('loveletter_create')->form();
+        $form->setValues(['loveletter' => [
+                'title' => 'Love letter',
+                'content' => 'Some link to [[Luke]]',
+                "player" => "ABCD",
+                "drama" => "789",
+                "roll1" => ["trait" => "Agilité", "difficulty" => -1],
+                "roll2" => ["trait" => "Agilité", "difficulty" => -1],
+                "roll3" => ["trait" => "Agilité", "difficulty" => -1],
+                "choice1" => "1",
+                "choice2" => "2",
+                "choice3" => "3",
+                "choice4" => "4"
+        ]]);
+        $this->client->submit($form);
+        $this->assertResponseRedirects();
+        $this->client->followRedirect();
+    }
+
+    public function testCreateWithTitle()
+    {
+        $crawler = $this->client->request('GET', '/loveletter/create?title=alderaan');
+        $form = $crawler->selectButton('loveletter_create')->form();
+        $this->assertEquals('Alderaan', $form['loveletter']['title']->getValue());
+    }
+
+    public function testList()
+    {
+        $crawler = $this->client->request('GET', '/vertex/filter');
+        $this->assertResponseIsSuccessful();
+        $url = $crawler->filterXPath('//nav/a/i[@class="icon-eye"]/parent::a')->attr('href');
+
+        return $url;
+    }
+
+    /** @depends testList */
+    public function testShow(string $show)
+    {
+        $crawler = $this->client->request('GET', $show);
+        $this->assertPageTitleContains('Love letter');
+        $url = $crawler->filterXPath('//nav/a/i[@class="icon-edit"]/parent::a')->attr('href');
+
+        return $url;
+    }
+
+    /** @depends testShow */
+    public function testEdit(string $edit)
+    {
+        $crawler = $this->client->request('GET', $edit);
+        $this->assertPageTitleContains('Love letter');
+        $this->assertCount(1, $crawler->selectButton('loveletter_create'));
+    }
+
+}
