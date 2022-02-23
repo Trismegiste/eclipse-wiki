e256595f8eac8626f6ff05a631fea6ecd17ba45b
Trismegiste <flo@trismegiste.net>
2022-02-23 12:07:18 +0100

    remove the mandatory channel for bluetooth

--- src/Service/ObjectPushFactory.php
+++ src/Service/ObjectPushFactory.php
@@ -8,6 +8,8 @@ namespace App\Service;
 
 use App\Entity\BtMessage;
 use App\Repository\BluetoothMsgRepository;
+use RuntimeException;
+use Symfony\Component\Process\Process;
 
 /**
  * Create Bluetooth OPUSH message
@@ -16,21 +18,41 @@ class ObjectPushFactory
 {
 
     protected $deviceMac;
-    protected $deviceChannel;
+    protected $deviceChannel = null;
     protected $repository;
 
-    public function __construct(string $mac, int $chan, BluetoothMsgRepository $repo)
+    public function __construct(string $mac, BluetoothMsgRepository $repo)
     {
-        $this->deviceChannel = $chan;
         $this->deviceMac = $mac;
         $this->repository = $repo;
     }
 
     public function send(string $filename): void
     {
+        if (is_null($this->deviceChannel)) {
+            $this->deviceChannel = $this->getChannelFor($this->deviceMac); // should be in construct but slow
+        }
+
         $msg = new BtMessage($this->deviceMac, $this->deviceChannel);
         $msg->body = $filename;
         $this->repository->save($msg);
+    }
+
+    protected function getChannelFor(string $btAddr): int
+    {
+        $scan = new Process([
+            'sdptool',
+            'search',
+            '--bdaddr', $btAddr,
+            'OPUSH'
+        ]);
+        $scan->mustRun();
+        $dump = $scan->getOutput();
+        if (!preg_match('#Channel:\s+(\d{1,2})#', $dump, $matches)) {
+            throw new RuntimeException("Could not find the OPUSH channel for $btAddr");
+        }
+
+        return (int) $matches[1];
     }
 
 }
