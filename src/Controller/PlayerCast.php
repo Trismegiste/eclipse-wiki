cf838a5f6e9278262e4a082fe9b448dc0f30c78d
Trismegiste <flo@trismegiste.net>
2022-03-07 20:55:34 +0100

    qrcode page for playercast

--- src/Controller/PlayerCast.php
+++ src/Controller/PlayerCast.php
@@ -6,9 +6,11 @@
 
 namespace App\Controller;
 
+use App\Service\NetTools;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 
 /**
  * Ctrl for WebSocket-controled Player Screen
@@ -16,12 +18,35 @@ use Symfony\Component\Routing\Annotation\Route;
 class PlayerCast extends AbstractController
 {
 
+    protected $ntools;
+
+    public function __construct(NetTools $utils)
+    {
+        $this->ntools = $utils;
+    }
+
     /**
      * @Route("/player", methods={"GET"})
      */
-    public function playerView(\App\Service\NetTools $utils): Response
+    public function view(): Response
     {
-        return $this->render('player/view.html.twig', ['host' => $utils->getLocalIp()]);
+        return $this->render('player/view.html.twig', ['host' => $this->getWebsocketHost()]);
+    }
+
+    /**
+     * @Route("/player/qrcode", methods={"GET"})
+     */
+    public function qrCode(): Response
+    {
+        $url = $this->generateUrl('app_playercast_view', [], UrlGeneratorInterface::ABSOLUTE_URL);
+        $lan = preg_replace('#//localhost#', '//' . $this->ntools->getLocalIp(), $url);
+
+        return $this->render('player/qrcode.html.twig', ['url_cast' => $lan]);
+    }
+
+    protected function getWebsocketHost(): string
+    {
+        return 'ws://' . $this->ntools->getLocalIp() . ':8889';
     }
 
 }
--- src/Service/NetTools.php
+++ src/Service/NetTools.php
@@ -16,8 +16,17 @@ class NetTools
     public function getLocalIp(): string
     {
         $name = '127.0.0.1';
+
         $sock = socket_create(AF_INET, SOCK_DGRAM, SOL_UDP);
-        socket_connect($sock, "8.8.8.8", 53);
+        if ($sock === false) {
+            throw new \RuntimeException("socket_create() failed. Reason: " . socket_strerror(socket_last_error()));
+        }
+
+        $ret = @socket_connect($sock, "8.8.8.8", 53);
+        if ($ret === false) {
+            throw new \RuntimeException("socket_connect() failed. Reason: " . socket_strerror(socket_last_error()));
+        }
+
         socket_getsockname($sock, $name); // $name passed by reference
         socket_close($sock);
 
