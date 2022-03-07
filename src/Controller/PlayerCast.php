80dbfffb69729c783b94939fa4236049873529e4
Trismegiste <flo@trismegiste.net>
2022-03-07 13:17:46 +0100

    player screen

---
+++ src/Controller/PlayerCast.php
@@ -0,0 +1,27 @@
+<?php
+
+/*
+ * eclipse-wiki
+ */
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * Ctrl for WebSocket-controled Player Screen
+ */
+class PlayerCast extends AbstractController
+{
+
+    /**
+     * @Route("/player", methods={"GET"})
+     */
+    public function playerView(): Response
+    {
+        return $this->render('player/view.html.twig');
+    }
+
+}
