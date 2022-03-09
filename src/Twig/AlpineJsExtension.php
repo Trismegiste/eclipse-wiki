de5237837eb57d7b869e4933a2bfcecd07fed8ec
Trismegiste <flo@trismegiste.net>
2022-03-09 22:18:38 +0100

    flashes fully javascripted

---
+++ src/Twig/AlpineJsExtension.php
@@ -0,0 +1,38 @@
+<?php
+
+/*
+ * Eclipse Wiki
+ */
+
+namespace App\Twig;
+
+use Twig\Extension\AbstractExtension;
+use Twig\TwigFunction;
+
+/**
+ * Extensions for AlpineJs
+ */
+class AlpineJsExtension extends AbstractExtension
+{
+
+    public function getFunctions()
+    {
+        return [
+            new TwigFunction('alpinejs_flashes', [$this, 'flattenFlashBag'])
+        ];
+    }
+
+    public function flattenFlashBag(array $bag): array
+    {
+        $flat = [];
+
+        foreach ($bag as $level => $listing) {
+            foreach ($listing as $flash) {
+                $flat[] = ['level' => $level, 'message' => $flash];
+            }
+        }
+
+        return ['flashes' => $flat];
+    }
+
+}
