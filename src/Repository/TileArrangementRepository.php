a4b8f61e6f308ac6f77759e264e1e296332c2ebd
Trismegiste <flo@trismegiste.net>
2022-06-15 13:59:45 +0200

    with persistence

---
+++ src/Repository/TileArrangementRepository.php
@@ -0,0 +1,15 @@
+<?php
+
+/*
+ * eclipse-wiki
+ */
+
+namespace App\Repository;
+
+/**
+ * Repository for TileArrangement
+ */
+class TileArrangementRepository extends \Trismegiste\Strangelove\MongoDb\DefaultRepository
+{
+
+    }
