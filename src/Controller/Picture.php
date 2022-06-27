3481fb8cd88c6aeec031694ac4c61bb62ba0f907
Trismegiste <flo@trismegiste.net>
2022-06-27 09:48:38 +0200

    create new controller for profile picture

--- src/Controller/Picture.php
+++ src/Controller/Picture.php
@@ -6,18 +6,13 @@
 
 namespace App\Controller;
 
-use App\Form\ProfilePic;
-use App\Repository\VertexRepository;
-use App\Service\AvatarMaker;
 use App\Service\PlayerCastCache;
 use App\Service\Storage;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\File\UploadedFile;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use function join_paths;
 
 /**
  * Controller for pictures
@@ -47,34 +42,6 @@ class Picture extends AbstractController
         }
 
         return new JsonResponse($choice);
-    }
-
-    /**
-     * Create an avatar for NPC
-     * @Route("/profile/create/{pk}", methods={"GET","POST"}, requirements={"pk"="[\da-f]{24}"})
-     */
-    public function profile(string $pk, Request $request, VertexRepository $repo, AvatarMaker $maker): Response
-    {
-        $npc = $repo->findByPk($pk);
-        $form = $this->createForm(ProfilePic::class, $npc);
-
-        $form->handleRequest($request);
-        if ($form->isSubmitted() && $form->isValid()) {
-            /** @var UploadedFile $avatarFile */
-            $avatarFile = $form->get('avatar')->getData();
-            $profilePic = $maker->generate($npc, imagecreatefromstring($avatarFile->getContent()));
-            $filename = $npc->getTitle() . '-avatar.png';
-            imagepng($profilePic, join_paths($this->storage->getRootDir(), $filename));
-            if (!$npc->hasAvatarSection()) {
-                $npc->appendAvatarSection($filename);
-                $repo->save($npc);
-            }
-            $this->addFlash('success', 'Profil réseaux sociaux généré');
-
-            return new JsonResponse('', Response::HTTP_NO_CONTENT);
-        }
-
-        return $this->render('picture/profile.html.twig', ['form' => $form->createView()]);
     }
 
     /**
