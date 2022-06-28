c4663df264a0d2ccd61ad7811b35312ad12f5c72
Trismegiste <flo@trismegiste.net>
2022-06-28 17:44:09 +0200

    command for generating link but problem with query and finding the right firewall

---
+++ src/Command/LoginLink.php
@@ -0,0 +1,46 @@
+<?php
+
+/*
+ * eclipse-wiki
+ */
+
+namespace App\Command;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Core\User\UserProviderInterface;
+use Symfony\Component\Security\Http\LoginLink\LoginLinkHandlerInterface;
+
+/**
+ * Generates a magic link to connect
+ */
+class LoginLink extends Command
+{
+
+    protected static $defaultName = 'app:link';
+    protected $handler;
+    protected $provider;
+
+    public function __construct(LoginLinkHandlerInterface $loginLinkHandler, UserProviderInterface $repo)
+    {
+        parent::__construct();
+        $this->handler = $loginLinkHandler;
+        $this->provider = $repo;
+    }
+
+    public function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $user = $this->provider->loadUserByIdentifier('gamemaster');
+        $request = Request::create('/');
+
+        $loginLinkDetails = $this->handler->createLoginLink($user, $request);
+        $loginLink = $loginLinkDetails->getUrl();
+
+        $output->writeln($loginLink);
+
+        return self::SUCCESS;
+    }
+
+}
