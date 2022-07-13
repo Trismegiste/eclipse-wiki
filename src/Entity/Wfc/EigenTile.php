14532284db3b62c9256eb02b4f53a9f8fae4679f
Trismegiste <flo@trismegiste.net>
2022-07-13 16:53:22 +0200

    refactor filename in eigen tile

--- src/Entity/Wfc/EigenTile.php
+++ src/Entity/Wfc/EigenTile.php
@@ -12,8 +12,29 @@ namespace App\Entity\Wfc;
 class EigenTile
 {
 
-    public $filename;  // the filename of the tile
-    public $rotation;  // the rotation of the file (in degrees)
+    protected $template;  // the filename of the tile
+    protected $rotation;  // the rotation of the file (in degrees)
     public $neighbourList = [];  // the 6 neighbour lists of EigenTile from HexagonalTile::EAST to HexagonalTile::SOUTHEAST
+
+    public function __construct(string $templateName, int $rotation = 0)
+    {
+        $this->template = $templateName;
+        $this->rotation = $rotation;
+    }
+
+    public function getUniqueId(): string
+    {
+        return $this->template . '-' . $this->rotation;
+    }
+
+    public function getTemplate(): string
+    {
+        return $this->template;
+    }
+
+    public function getRotation(): int
+    {
+        return $this->rotation;
+    }
 
 }
