4b1d3d1895e637866c2c448fea90619d1bd696f8
Trismegiste <flo@trismegiste.net>
2022-08-28 12:23:18 +0200

    info on row

--- src/Voronoi/Shape/Strategy.php
+++ src/Voronoi/Shape/Strategy.php
@@ -6,15 +6,19 @@
 
 namespace App\Voronoi\Shape;
 
+use App\Voronoi\MapDrawer;
+use MongoDB\BSON\Persistable;
+use Trismegiste\Strangelove\MongoDb\PersistableImpl;
+
 /**
  * Drawing the global shape of the voronoi map
  */
-abstract class Strategy implements \MongoDB\BSON\Persistable
+abstract class Strategy implements Persistable
 {
 
-    use \Trismegiste\Strangelove\MongoDb\PersistableImpl;
+    use PersistableImpl;
 
-    abstract public function draw(\App\Voronoi\MapDrawer $draw): void;
+    abstract public function draw(MapDrawer $draw): void;
 
     abstract function getName(): string;
 }
