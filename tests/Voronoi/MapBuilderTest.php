c4511284da1f31f174f9209893e893d334c52206
Trismegiste <flo@trismegiste.net>
2022-08-31 10:58:57 +0200

    unit test for MapBuilder

--- tests/Voronoi/MapBuilderTest.php
+++ tests/Voronoi/MapBuilderTest.php
@@ -4,6 +4,9 @@
  * eclipse-wiki
  */
 
+use App\Entity\MapConfig;
+use App\Entity\Shape\NullShape;
+use App\Voronoi\HexaMap;
 use App\Voronoi\MapBuilder;
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
 
@@ -15,6 +18,52 @@ class MapBuilderTest extends KernelTestCase
     protected function setUp(): void
     {
         $this->sut = static::getContainer()->get(MapBuilder::class);
+    }
+
+    public function getConfig()
+    {
+        $config = new MapConfig('essai');
+        $config->side = 20;
+        $config->seed = 111;
+        $config->avgTilePerRoom = 15;
+        $config->horizontalLines = 1;
+        $config->verticalLines = 1;
+        $config->container = new NullShape();
+
+        return [[$config]];
+    }
+
+    /** @dataProvider getConfig */
+    public function testCreate(MapConfig $config)
+    {
+        $map = $this->sut->create($config);
+        $this->assertInstanceOf(HexaMap::class, $map);
+        $this->assertEquals(20, $map->getSize());
+    }
+
+    /** @dataProvider getConfig */
+    public function testSvg(MapConfig $config)
+    {
+        $map = $this->sut->create($config);
+        ob_start();
+        $this->sut->dumpSvg($map);
+        $svg = ob_get_clean();
+        $this->assertStringContainsString('<svg', $svg);
+        $this->assertStringEndsWith('</svg>', $svg);
+    }
+
+    /** @dataProvider getConfig */
+    public function testSaveFile(MapConfig $config)
+    {
+        $map = $this->sut->create($config);
+        $pathname = __DIR__ . '/yolo.svg';
+
+        @unlink($pathname);
+        $this->assertFileDoesNotExist($pathname);
+        $this->sut->save($map, $pathname);
+        $this->assertFileExists($pathname);
+        $this->assertFileIsReadable($pathname);
+        unlink($pathname);
     }
 
 }
