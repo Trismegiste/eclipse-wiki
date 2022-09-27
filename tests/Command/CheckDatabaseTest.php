d89b541673e93e60bead7475a57a168ffe6804eb
Trismegiste <flo@trismegiste.net>
2022-09-27 17:24:06 +0200

    func test for command check database

---
+++ tests/Command/CheckDatabaseTest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Command;
+
+use Symfony\Bundle\FrameworkBundle\Console\Application;
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+use Symfony\Component\Console\Tester\CommandTester;
+
+class CheckDatabaseTest extends KernelTestCase
+{
+    public function testExecute()
+    {
+        $kernel = self::bootKernel();
+        $application = new Application($kernel);
+
+        $command = $application->find('app:check:mongo');
+        $commandTester = new CommandTester($command);
+        $commandTester->execute([]);
+
+        // the output of the command in the console
+        $output = $commandTester->getDisplay();
+        $this->assertStringContainsString('configuration', $output);
+    }
+}
