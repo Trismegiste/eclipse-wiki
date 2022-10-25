c0e0ca04d441ee0b70b19a39470fe9375c8ae483
Trismegiste <flo@trismegiste.net>
2022-10-25 15:59:13 +0200

    more coverage

--- tests/Service/PlayerCastCacheTest.php
+++ tests/Service/PlayerCastCacheTest.php
@@ -13,11 +13,26 @@ class PlayerCastCacheTest extends KernelTestCase
         $this->sut = static::getContainer()->get(PlayerCastCache::class);
     }
 
-    public function testSlimPicture()
+    public function getSizeConfig()
     {
-        $side = 2500;
+         return [[700],[1100]];
+    }
+
+    /** @dataProvider getSizeConfig */
+    public function testSlimPicture(int $side)
+    {
         $big = imagecreatetruecolor($side, $side);
         $target = sys_get_temp_dir() . '/big.png';
+
+        $white = imagecolorallocate($big, 255, 255, 255);
+        for($x=0;$x<$side;$x++) {
+            for($y=0;$y<$side;$y++) {
+                 if (rand() % 2) { // a lot of noise to mess with PNG compression
+                      imagesetpixel($big, $x, $y, $white);
+                 }
+            }
+        }
+
         imagepng($big, $target);
         $this->assertGreaterThan(1e5, filesize($target));
 
