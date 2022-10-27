ad1c19ee691437a72998eb5aed7709a9f9e9a5d3
Trismegiste <flo@trismegiste.net>
2022-10-27 16:22:14 +0200

    form for posting avatar

--- src/Controller/ProfilePicture.php
+++ src/Controller/ProfilePicture.php
@@ -72,7 +72,7 @@ class ProfilePicture extends AbstractController
     }
 
     /**
-     * Show a list of NPC profiles for a Place
+     * Show a list of NPC profiles from a template (a Transhuman with isNpcTemplate() method returns true)
      * @Route("/profile/template/{pk}", methods={"GET"}, requirements={"pk"="[\da-f]{24}"})
      */
     public function template(string $pk): Response
@@ -148,15 +148,30 @@ class ProfilePicture extends AbstractController
         return $this->render('picture/profile.html.twig', ['form' => $form->createView()]);
     }
 
-    /** @Route("/profile/bauhaus") */
-    public function index(BoringAvatar $maker): Response
+    /**
+     * Show a list of NPC profiles from a template (a Transhuman with isNpcTemplate() method returns true)
+     * @Route("/profile/bauhaus/{pk}", methods={"GET", "POST"}, requirements={"pk"="[\da-f]{24}"})
+     */
+    public function bauhaus(string $pk, Request $request, BoringAvatar $maker): Response
     {
+        $vertex = $this->repository->findByPk($pk);
+        $form = $this->createForm(ProfileOnTheFly::class);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            // which button : push or create ?
+        }
+
         $bauhaus = [];
         for ($k = 0; $k < 24; $k++) {
             $bauhaus[] = $maker->createBauhaus('yolo' . rand());
         }
 
-        return $this->render('picture/profile_instantiate.html.twig', ['bauhaus' => $bauhaus]);
+        return $this->render('picture/profile_instantiate.html.twig', [
+                    'form' => $form->createView(),
+                    'bauhaus' => $bauhaus,
+                    'npc' => $vertex
+        ]);
     }
 
 }
