ff7d9ff5078cbb94e3c32183f4c1aedbf9ec28b8
Trismegiste <flo@trismegiste.net>
2022-10-28 21:04:18 +0200

    widget avatar choice

--- src/Form/ProfileOnTheFly.php
+++ src/Form/ProfileOnTheFly.php
@@ -8,12 +8,9 @@ namespace App\Form;
 
 use App\Entity\Transhuman;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\Form\FormInterface;
-use Symfony\Component\Form\FormView;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 /**
@@ -22,17 +19,13 @@ use Symfony\Component\OptionsResolver\OptionsResolver;
 class ProfileOnTheFly extends AbstractType
 {
 
-    protected $bauhausAvatar;
-
-    public function __construct(\App\Service\BoringAvatar $maker)
-    {
-        $this->bauhausAvatar = $maker;
-    }
-
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-                ->add('avatar', FileType::class, ['mapped' => false])
+                ->add('avatar', Type\AvatarChoiceType::class, [
+                    'mapped' => false,
+                    'bitmap' => $options['data']->tokenPic
+                ])
                 ->add('title', TextType::class)
                 ->add('generate', SubmitType::class)
         ;
@@ -41,15 +34,6 @@ class ProfileOnTheFly extends AbstractType
     public function configureOptions(OptionsResolver $resolver)
     {
         $resolver->setDefault('data_class', Transhuman::class);
-    }
-
-    public function finishView(FormView $view, FormInterface $form, array $options)
-    {
-        $bauhaus = [];
-        for ($k = 0; $k < 24; $k++) {
-            $bauhaus[] = $this->bauhausAvatar->createBauhaus('yolo' . rand());
-        }
-        $view->vars['bauhaus'] = $bauhaus;
     }
 
 }
---
+++ src/Form/Type/AvatarChoiceType.php
@@ -0,0 +1,58 @@
+<?php
+
+/*
+ * Eclipse Wiki
+ */
+
+namespace App\Form\Type;
+
+use App\Service\BoringAvatar;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\FormInterface;
+use Symfony\Component\Form\FormView;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+/**
+ * Description of AvatarChoiceType
+ *
+ * @author florent
+ */
+class AvatarChoiceType extends AbstractType
+{
+
+    protected $bauhausAvatar;
+
+    public function __construct(BoringAvatar $maker)
+    {
+        $this->bauhausAvatar = $maker;
+    }
+
+    public function getParent()
+    {
+        return FileType::class;
+    }
+
+    public function finishView(FormView $view, FormInterface $form, array $options)
+    {
+        $bauhaus = [];
+        for ($k = 0; $k < $options['abstract_number']; $k++) {
+            $bauhaus[] = $this->bauhausAvatar->createBauhaus('yolo' . rand());
+        }
+        $view->vars['bauhaus'] = $bauhaus;
+        $view->vars['avatar_size'] = $options['avatar_size'];
+        $view->vars['default_bitmap'] = $options['bitmap'];
+        $view->vars['human_number'] = $options['human_number'];
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'bitmap' => null,
+            'avatar_size' => 503,
+            'abstract_number' => 24,
+            'human_number' => 3
+        ]);
+    }
+
+}
