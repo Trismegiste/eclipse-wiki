4c13f97222d71f281042a7390af1b265cf8c8755
Trismegiste <flo@trismegiste.net>
2022-11-09 15:22:27 +0100

    updating packages and removing useless bundles

--- config/bundles.php
+++ config/bundles.php
@@ -8,7 +8,6 @@ return [
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
     Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true],
-    Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Knp\Bundle\SnappyBundle\KnpSnappyBundle::class => ['all' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Trismegiste\Strangelove\TrismegisteStrangeloveBundle::class => ['all' => true],
