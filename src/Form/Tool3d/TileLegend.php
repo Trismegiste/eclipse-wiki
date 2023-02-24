12e5dff16b51e86d9cf81892f8a5d6a333c6ca1a
Trismegiste <flo@trismegiste.net>
2023-02-24 14:51:26 +0100

    cache config

--- src/Form/Tool3d/TileLegend.php
+++ src/Form/Tool3d/TileLegend.php
@@ -8,7 +8,7 @@ namespace App\Form\Tool3d;
 
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
-use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -21,10 +21,10 @@ class TileLegend extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-                ->add('legend', TextareaType::class, [
+                ->add('legend', TextType::class, [
                     'attr' => ['x-model' => 'cellInfo.legend']
                 ])
-                ->add('name', SubmitType::class)
+                ->add('set_legend', SubmitType::class)
         ;
     }
 
