09f43fda70a2e6de2df5098d7dffabf56984026f
Trismegiste <flo@trismegiste.net>
2023-03-16 09:45:37 +0100

    pb with infobox and flex

--- src/Command/BookGenerate.php
+++ src/Command/BookGenerate.php
@@ -6,7 +6,9 @@
 
 namespace App\Command;
 
+use Knp\Snappy\Pdf;
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
@@ -20,11 +22,20 @@ class BookGenerate extends Command
 
     protected static $defaultName = 'book:generate';
     protected Environment $twig;
+    protected Pdf $pdfWriter;
 
-    public function __construct(Environment $twig)
+    public function __construct(Environment $twig, Pdf $pdfWriter)
     {
         parent::__construct();
         $this->twig = $twig;
+        $this->pdfWriter = $pdfWriter;
+    }
+
+    protected function configure()
+    {
+        $this->setDescription('Generate a book from the MW')
+                ->addArgument('target', InputArgument::REQUIRED)
+        ;
     }
 
     protected function execute(InputInterface $input, OutputInterface $output): int
@@ -32,7 +43,11 @@ class BookGenerate extends Command
         $io = new SymfonyStyle($input, $output);
         $io->title('Book');
 
-        file_put_contents('index.html', $this->twig->render('book/Eclipse Phase.html.twig', ['titre' => 'Eclipse Phase']));
+        $this->pdfWriter->generateFromHtml(
+                $this->twig->render('book/Eclipse Phase.html.twig', ['titre' => 'Eclipse Phase']),
+                $input->getArgument('target'),
+                ['page-size' => 'A5']
+        );
 
         return self::SUCCESS;
     }
