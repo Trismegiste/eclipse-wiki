84cd5bfe5ef0c718ab3f8c96336c227be0ba932a
Trismegiste <flo@trismegiste.net>
2023-04-27 10:36:24 +0200

    adding purge

--- src/Command/PictureCensus.php
+++ src/Command/PictureCensus.php
@@ -8,9 +8,11 @@ namespace App\Command;
 
 use App\Repository\VertexRepository;
 use App\Service\Storage;
+use MongoDB\BSON\Regex;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
 
@@ -31,20 +33,38 @@ class PictureCensus extends Command
         $this->store = $store;
     }
 
+    protected function configure()
+    {
+        $this->addOption('purge', null, InputOption::VALUE_NONE, 'Delete pictures');
+    }
+
     public function execute(InputInterface $input, OutputInterface $output): int
     {
         $io = new SymfonyStyle($input, $output);
         $io->title('Search for unused pictures');
 
         $unused = 0;
-        foreach ($this->store->searchByName('(jpg|jpeg|png|webp|gif)') as $pic) {
+        foreach ($this->store->searchByName('(jpg|jpeg|png|webp|gif|svg|json)') as $pic) {
+            /** @var \SplFileInfo $pic */
+            // we can find picture :
             $found = $this->repo->searchOne(['$or' => [
-                    ['content' => new \MongoDB\BSON\Regex(preg_quote($pic->getBasename()))],
-                    ['tokenPic' => $pic->getBasename()]
+                    // in all vertices content
+                    ['content' => new Regex(preg_quote($pic->getBasename()))],
+                    // in character avatar
+                    ['tokenPic' => $pic->getBasename()],
+                    // in loveletter drama field
+                    ['drama' => new Regex(preg_quote($pic->getBasename()))],
+                    // in handout gm info field
+                    ['gmInfo' => new Regex(preg_quote($pic->getBasename()))],
+                    // in place battlemap field
+                    ['battlemap3d' => $pic->getBasename()],
             ]]);
             if (is_null($found)) {
                 $io->writeln($pic->getBasename());
                 $unused++;
+                if ($input->getOption('purge')) {
+                    @unlink($pic->getPathname());
+                }
             }
         }
         $io->comment("$unused files not used");
