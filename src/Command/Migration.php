66715a9430b871a0e8783ca714b8d2250e8be62d
Trismegiste <flo@trismegiste.net>
2023-06-20 01:00:51 +0200

    fine tune the migration

--- src/Command/Migration.php
+++ src/Command/Migration.php
@@ -6,17 +6,15 @@
 
 namespace App\Command;
 
+use App\Entity\PlotNode;
+use App\Entity\Timeline;
 use App\Repository\VertexRepository;
-use App\Service\Storage;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
-use Symfony\Component\Console\Exception\RuntimeException;
-use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
-use Symfony\Component\Finder\SplFileInfo;
-use App\Entity\Timeline;
+use function str_starts_with;
 
 /**
  * Migrate database
@@ -60,15 +58,15 @@ class Migration extends Command
                 }
 
                 if ($startRecording && str_starts_with($row, '*')) {
-                    $timeline[] = str_replace(['{{task|', '|checked', '}}', '<strike>', '</strike>'], ['', '', '', '', ''], trim(substr($row, 1)));
+                    $timeline[] = str_replace(['{{task|', '|checked', '}}', '<strike>', '</strike>', '<s>', '</s>'], ['', '', '', '', ''], trim(substr($row, 1)));
                 }
             }
-            $tree = new \App\Entity\PlotNode('Root');
+            $tree = new PlotNode('Root');
             foreach ($timeline as $node) {
-                $tree->nodes[] = new \App\Entity\PlotNode($node);
+                $tree->nodes[] = new PlotNode($node);
             }
             $vertex->setTree($tree);
-            var_dump($vertex->bsonSerialize());
+            $this->repo->save($vertex);
         }
         $io->success('End of migration');
 
