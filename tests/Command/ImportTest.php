8c2c69b9f5eae7105c7211cde690df1a57329d1e
Trismegiste <flo@trismegiste.net>
2023-06-20 15:51:07 +0200

    building the export.zip on the fly

--- tests/Command/ImportTest.php
+++ tests/Command/ImportTest.php
@@ -11,21 +11,46 @@ use Symfony\Component\Console\Tester\CommandTester;
 class ImportTest extends KernelTestCase
 {
 
-    public function testExecute()
+    public function getExportZip()
     {
-        $target = __DIR__ . '/export.zip';
+        $filename = 'export-tmp.zip';
+        $zip = new ZipArchive();
+        $zip->open($filename, ZipArchive::CREATE | ZipArchive::OVERWRITE);
+
+        $timeline = new App\Entity\Timeline('scenar');
+        $timeline->elevatorPitch = 'For export';
+        $timeline->setTree(new App\Entity\PlotNode('scene 1'));
+        $export = \MongoDB\BSON\toJSON(\MongoDB\BSON\fromPHP([$timeline]));
+        $zip->addFromString(App\Command\Export::vertexFilename, $export);
+
+        $img = imagecreatetruecolor(256, 256);
+        $tmpfname = tempnam(__DIR__, 'export');
+        imagejpeg($img, $tmpfname);
+        $zip->addFile($tmpfname, 'img.jpg');
+
+        $zip->close();
+        unlink($tmpfname);
+
+        return [[$filename]];
+    }
+
+    /** @dataProvider getExportZip */
+    public function testExecute(string $archive)
+    {
+        $this->assertFileExists($archive);
         $kernel = self::bootKernel();
         $application = new Application($kernel);
 
         $command = $application->find('db:import');
         $commandTester = new CommandTester($command);
         $commandTester->setInputs(['yes']);
-        $commandTester->execute(['source' => $target]);
+        $commandTester->execute(['source' => $archive]);
 
         // the output of the command in the console
         $output = $commandTester->getDisplay();
         $this->assertStringContainsString('Import succeed', $output);
-        $this->assertFileExists($target);
+
+        unlink($archive);
     }
 
 }
