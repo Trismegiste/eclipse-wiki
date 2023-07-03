6cf1d48931f8c809b4919f4b1720c7a64f5f8060
Trismegiste <flo@trismegiste.net>
2023-07-03 16:31:59 +0200

    unit test for morph repo

--- tests/Repository/MorphProviderTest.php
+++ tests/Repository/MorphProviderTest.php
@@ -4,32 +4,76 @@
  * eclipse-wiki
  */
 
+use App\Repository\EdgeProvider;
+use App\Repository\HindranceProvider;
 use App\Repository\MorphProvider;
 use App\Service\MediaWiki;
 use PHPUnit\Framework\TestCase;
-use Symfony\Contracts\Cache\CacheInterface;
+use Symfony\Component\Cache\Adapter\ArrayAdapter;
 
 class MorphProviderTest extends TestCase
 {
 
     protected $sut;
 
+    protected function buildTypeMorphDocument()
+    {
+        $doc = new DOMDocument();
+        $doc->loadHTML('<ul><li><a href="Catégorie:Biomorphe">Biomorphe</a></li></ul>');
+
+        return $doc;
+    }
+
+    protected function buildMorphDocument()
+    {
+        $doc = new DOMDocument();
+        $doc->loadHTML('<div data-source="type"><div>Biomorphe</div></div><div data-source="cout"><div>3</div></div>');
+
+        return $doc;
+    }
+
+    protected function buildMorphTree()
+    {
+        return new DOMDocument();
+    }
+
     protected function setUp(): void
     {
         $api = $this->createMock(MediaWiki::class);
-        $cache = $this->createMock(CacheInterface::class);
-        $cache->expects($this->once())
-            ->method('get')
-            ->willReturn([]);
-        $ep = $this->createStub(App\Repository\EdgeProvider::class);
-        $hp = $this->createStub(App\Repository\HindranceProvider::class);
+        $api->expects($this->any())
+                ->method('getDocumentByName')
+                ->willReturnMap([
+                    ['Type de Morphe', $this->buildTypeMorphDocument()]
+        ]);
+
+        $api->expects($this->any())
+                ->method('searchPageFromCategory')
+                ->willReturn([(object) ['title' => 'Basique']]);
+
+        $api->expects($this->any())
+                ->method('getTreeAndHtmlDomByName')
+                ->willReturn(['html' => $this->buildMorphDocument(), 'tree' => $this->buildMorphTree()]);
+
+        $cache = new ArrayAdapter();
+        //  $cache = new Symfony\Component\Cache\Adapter\NullAdapter();
+        $ep = $this->createStub(EdgeProvider::class);
+        $hp = $this->createStub(HindranceProvider::class);
 
         $this->sut = new MorphProvider($api, $cache, $ep, $hp);
     }
 
     public function testListing()
     {
-        $this->assertIsArray($this->sut->getListing());
+        $listing = $this->sut->getListing();
+        $this->assertIsArray($listing);
+        $this->assertCount(1, $listing);
+        $this->assertCount(1, $listing['Biomorphe']);
+    }
+
+    public function testFindOne()
+    {
+        $morph = $this->sut->findOne('Basique');
+        $this->assertInstanceOf(\App\Entity\Morph::class, $morph);
     }
 
 }
