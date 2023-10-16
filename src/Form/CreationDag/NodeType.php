4bc2aee770425f132f42d0340f1ad52b49a8ae20
Trismegiste <flo@trismegiste.net>
2023-10-16 09:54:08 +0200

    creation form ok

--- src/Form/CreationDag/NodeType.php
+++ src/Form/CreationDag/NodeType.php
@@ -10,7 +10,9 @@ use App\Entity\CreationTree\Node;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\NotBlank;
 
 /**
  * Form for a Node
@@ -21,7 +23,7 @@ class NodeType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-                ->add('name', TextType::class)
+                ->add('name', TextType::class, ['constraints' => [new NotBlank()]])
                 ->add('children', NodeLinkType::class, [
                     'multiple' => true,
                     'expanded' => true,
@@ -34,6 +36,9 @@ class NodeType extends AbstractType
     {
         $resolver->setDefault('data_class', Node::class);
         $resolver->setRequired('graph');
+        $resolver->setDefault('empty_data', function (FormInterface $form) {
+            return new Node($form['name']->getData());
+        });
     }
 
 }
