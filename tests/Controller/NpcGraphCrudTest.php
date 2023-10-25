939c5ff11d7e5c5a71efbf39d61d917f8533d8a9
Trismegiste <flo@trismegiste.net>
2023-10-25 10:52:51 +0200

    func test of edit npc graph

--- tests/Controller/NpcGraphCrudTest.php
+++ tests/Controller/NpcGraphCrudTest.php
@@ -21,11 +21,50 @@ class NpcGraphCrudTest extends WebTestCase
         $this->repository = static::getContainer()->get(VertexRepository::class);
     }
 
-    public function testEdit()
+    public function testReset()
+    {
+        $storage = static::getContainer()->get(\App\Service\Storage::class);
+        /** @var \App\Service\Storage $storage */
+        $configFile = join_paths($storage->getRootDir(), App\Repository\CreationGraphProvider::FILENAME);
+        @unlink($configFile);
+        $this->assertFileDoesNotExist($configFile);
+    }
+
+    public function testAddingNewNodes()
     {
         $crawler = $this->client->request('GET', '/npc/graph/edit');
         $this->assertResponseIsSuccessful();
         $this->assertSelectorExists('#full_tree_save');
+        $form = $crawler->selectButton('full_tree_save')->form();
+
+        $values = $form->getPhpValues();
+        $values['full_tree']['node'][0]['name'] = 'root';
+        $values['full_tree']['node'][1]['name'] = 'male';
+        $values['full_tree']['node'][2]['name'] = 'female';
+
+        $this->client->request($form->getMethod(), $form->getUri(), $values, $form->getPhpFiles());
+        $this->assertResponseRedirects();
+        $this->client->followRedirect();
+        $this->assertResponseIsSuccessful();
+    }
+
+    public function testLinkingNodes()
+    {
+        $crawler = $this->client->request('GET', '/npc/graph/edit');
+        $this->assertResponseIsSuccessful();
+        $elem = $crawler->filter('form.pure-form h2');
+        $this->assertCount(3, $elem);
+        $this->assertEquals('Root', $elem->eq(0)->text());
+        $this->assertEquals('Male', $elem->eq(1)->text());
+        $this->assertEquals('Female', $elem->eq(2)->text());
+
+        $form = $crawler->selectButton('full_tree_save')->form();
+        $values = $form->getPhpValues();
+        $values['full_tree']['node'][0]['children'] = ['male', 'female'];
+        $this->client->request($form->getMethod(), $form->getUri(), $values, $form->getPhpFiles());
+        $this->assertResponseRedirects();
+        $this->client->followRedirect();
+        $this->assertResponseIsSuccessful();
     }
 
     public function testRun()
