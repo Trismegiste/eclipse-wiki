5e61f714d91a8fb3d943f30b381bb533c27e89dd
Trismegiste <flo@trismegiste.net>
2023-11-12 20:34:13 +0100

    Sorting by ratio of missing / prompt counts

--- src/Command/StableDiffusion/MissingGraphPrompt.php
+++ src/Command/StableDiffusion/MissingGraphPrompt.php
@@ -35,39 +35,39 @@ class MissingGraphPrompt extends Command
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
         $io = new SymfonyStyle($input, $output);
-        $io->title("Iterates on keywords in the Creation Graph and search for missing keywords combinations");
+        $io->title("Generates all prompts from keywords in the Creation Graph and searches for missing prompts in InvokeAI local storage");
 
         $graph = $this->loader->load();
 
-        $io->section("Prompts that cannot match with local repository");
+        $io->section("Stats on most common keywords in unmatched prompts");
 
         $listing = $this->recurBuildPrompt($graph, [], $input->getArgument('root-name'));
+        $promptStats = [];
+
+        $io->progressStart(count($listing));
         foreach ($listing as $prompt) {
-            $io->text(implode(' ', $prompt));
-        }
-
-        $filtered = array_filter($listing, function (array $prompt): bool {
-            array_unique($prompt);
-            return !count($this->repository->searchPicture(implode(' ', $prompt)));
-        });
-
-        $io->section("Stats on most common keywords in unmatched prompts");
-        $stats = [];
-        foreach ($filtered as $keywords) {
-            foreach ($keywords as $word) {
-                if (!key_exists($word, $stats)) {
-                    $stats[$word] = 0;
+            foreach ($prompt as $word) {
+                if (!key_exists($word, $promptStats)) {
+                    $promptStats[$word] = ['count' => 0, 'missing' => 0];
                 }
-                $stats[$word]++;
+                $promptStats[$word]['count']++;
             }
+            $matching = $this->repository->searchPicture(implode(' ', $prompt));
+            if (count($matching) == 0) {
+                $promptStats[$word]['missing']++;
+            }
+            $io->progressAdvance();
         }
+        $io->progressFinish();
 
-        uasort($stats, function (int $a, int $b): int {
-            return $b <=> $a;
+        // sort
+        uasort($promptStats, function (array $a, array $b): int {
+            return ($b['missing'] / $b['count']) <=> ($a['missing'] / $a['count']);
         });
 
-        foreach ($stats as $word => $nb) {
-            $io->writeln("$word : $nb");
+        // print
+        foreach ($promptStats as $word => $counter) {
+            $io->writeln(sprintf("$word : %.0f%% (%d/%d)", 100 - $counter['missing'] * 100 / $counter['count'], $counter['count'] - $counter['missing'], $counter['count']));
         }
 
         return 0;
