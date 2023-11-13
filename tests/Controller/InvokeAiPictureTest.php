4bc27ef83ea3e292d02c270b8af4978b101fa175
Trismegiste <flo@trismegiste.net>
2023-11-13 09:45:20 +0100

    func tests on InvokeAI local search with a pictures

--- tests/Controller/InvokeAiPictureTest.php
+++ tests/Controller/InvokeAiPictureTest.php
@@ -2,6 +2,8 @@
 
 use App\Entity\Place;
 use App\Repository\VertexRepository;
+use App\Service\StableDiffusion\LocalRepository;
+use App\Tests\Service\StableDiffusion\PngReaderTest;
 use Symfony\Bundle\FrameworkBundle\KernelBrowser;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 use Trismegiste\Strangelove\MongoDb\Repository;
@@ -19,9 +21,21 @@ class InvokeAiPictureTest extends WebTestCase
     }
 
     public function testSearch()
-    {
-        $this->client->request('GET', '/invokeai/search');
+    {     // inbsert
+        $storage = self::getContainer()->get(LocalRepository::class);
+        $folder = __DIR__ . '/../fixtures';
+        $src = join_paths($folder, PngReaderTest::fixture);
+        $dst = join_paths($storage->getRootDir(), PngReaderTest::fixture);
+        copy($src, $dst);
+
+        $crawler = $this->client->request('GET', '/invokeai/search');
         $this->assertResponseIsSuccessful();
+        $this->assertSelectorExists('#form_search');
+        $form = $crawler->selectButton('form_search')->form();
+        $form->setValues(['form' => ['query' => 'strawberry']]);
+        $this->client->submit($form);
+        $this->assertSelectorCount(1, '.search .thumbnail img');
+        @unlink($dst);
     }
 
     public function testAppendPictureToPlace()
