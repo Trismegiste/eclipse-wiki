ad17afe56646477a15525d60f49d3c2d24a4c74f
Trismegiste <flo@trismegiste.net>
2023-11-14 14:53:59 +0100

    unit tet for factory of InvokeAi client

---
+++ tests/Service/StableDiffusion/InvokeAiClientFactoryTest.php
@@ -0,0 +1,37 @@
+<?php
+
+/*
+ * eclipse-wiki
+ */
+
+use App\Service\StableDiffusion\InvokeAi;
+use App\Service\StableDiffusion\InvokeAiClientFactory;
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\Cache\Adapter\ArrayAdapter;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
+
+class InvokeAiClientFactoryTest extends TestCase
+{
+
+    protected $sut;
+
+    protected function setUp(): void
+    {
+        $client = $this->createStub(HttpClientInterface::class);
+        $cache = new ArrayAdapter();
+        $this->sut = new InvokeAiClientFactory($client, $cache);
+    }
+
+    public function testFromHostname()
+    {
+        $api = $this->sut->createFromHostname('yolo.mars');
+        $this->assertInstanceOf(InvokeAi::class, $api);
+    }
+
+    public function testFromMac()
+    {
+        $api = $this->sut->createFromMac('01:02:03:04');
+        $this->assertInstanceOf(InvokeAi::class, $api);
+    }
+
+}
