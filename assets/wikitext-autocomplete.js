10d6fb09479b779dcb33f8ac25cb107f51057ab7
Trismegiste <flo@trismegiste.net>
2023-11-19 11:39:12 +0100

    module config for alpine, the right way

--- assets/wikitext-autocomplete.js
+++ assets/wikitext-autocomplete.js
@@ -2,95 +2,101 @@
  * Eclipse Wiki
  */
 import getCaretCoordinates from 'textarea-caret';
-export default {
-    caretChangingKey: ['Escape', 'ArrowLeft', 'ArrowRight', 'ArrowLeft', 'ArrowUp', 'ArrowDown', 'Home', 'End', 'PageUp', 'PageDown'],
-    content: '',
-    open: false,
-    caretPos: {top: 0, left: 0},
-    result: [],
-    selected: null,
-    strategy: [],
+export default (strategy) => {
+    return () => ({
+            caretChangingKey: ['Escape', 'ArrowLeft', 'ArrowRight', 'ArrowLeft', 'ArrowUp', 'ArrowDown', 'Home', 'End', 'PageUp', 'PageDown'],
+            content: '',
+            open: false,
+            caretPos: {top: 0, left: 0},
+            result: [],
+            selected: null,
+            strategy: strategy,
 
-    editKeyUp(event) {
-        // if the caret is changing position : stop the autocomplete
-        if (this.caretChangingKey.includes(event.key)) {
-            this.open = false
-            return;
-        }
+            init() {
+                this.content = this.$refs.editor.value.trim()
+            },
 
-        // iterator over strategies
-        let found = false
-        for (let strategy of this.strategy) {
-            let extract = strategy.pattern.exec(this.content.substring(0, this.getCaretPosition()))
-            if (null !== extract) {
-                found = true
-                // position the combobox if we match
-                if (!this.open) {
-                    let caretPosition = getCaretCoordinates(this.$refs.editor, this.$refs.editor.selectionEnd);
-                    let rect = this.$refs.editor.getBoundingClientRect()
-                    this.caretPos.top = (rect.top + caretPosition.top + caretPosition.height) + 'px'
-                    this.caretPos.left = (rect.left + caretPosition.left) + 'px'
+            editKeyUp(event) {
+                // if the caret is changing position : stop the autocomplete
+                if (this.caretChangingKey.includes(event.key)) {
+                    this.open = false
+                    return;
                 }
-                //fetching ajax
-                fetch(strategy.url + extract[1])
-                        .then((response) => {
-                            return response.json()
-                        })
-                        .then((data) => {
-                            this.result = []
-                            for (let entry of data) {
-                                let attr = strategy.boundAttr(entry)
-                                attr['value'] = entry
-                                this.result.push(attr)
-                            }
-                            // showing the combobox only if there are results
-                            this.open = (data.length > 0)
-                        })
-                break
-            }
-        }
-        if (!found) {
-            this.open = false
-        }
-    },
 
-    choose(event) {
-        let caret = this.getCaretPosition()
-        let before = this.content.substring(0, caret)
-        let after = this.content.substring(caret)
+                // iterator over strategies
+                let found = false
+                for (let strategy of this.strategy) {
+                    let extract = strategy.pattern.exec(this.content.substring(0, this.getCaretPosition()))
+                    if (null !== extract) {
+                        found = true
+                        // position the combobox if we match
+                        if (!this.open) {
+                            let caretPosition = getCaretCoordinates(this.$refs.editor, this.$refs.editor.selectionEnd);
+                            let rect = this.$refs.editor.getBoundingClientRect()
+                            this.caretPos.top = (rect.top + caretPosition.top + caretPosition.height) + 'px'
+                            this.caretPos.left = (rect.left + caretPosition.left) + 'px'
+                        }
+                        //fetching ajax
+                        fetch(strategy.url + extract[1])
+                                .then((response) => {
+                                    return response.json()
+                                })
+                                .then((data) => {
+                                    this.result = []
+                                    for (let entry of data) {
+                                        let attr = strategy.boundAttr(entry)
+                                        attr['value'] = entry
+                                        this.result.push(attr)
+                                    }
+                                    // showing the combobox only if there are results
+                                    this.open = (data.length > 0)
+                                })
+                        break
+                    }
+                }
+                if (!found) {
+                    this.open = false
+                }
+            },
 
-        for (let strategy of this.strategy) {
-            let pattern = strategy.pattern
-            let extract = pattern.exec(before)
-            if (null !== extract) {
-                let newValue = strategy.replace(this.selected) + ' '
-                caret += newValue.length - extract[0].length
-                this.content = pattern[Symbol.replace](before, newValue) + after
-                this.$nextTick(() => {
-                    this.$refs.editor.focus()
-                    this.$refs.editor.setSelectionRange(caret, caret)
-                })
-                break
-            }
-        }
-        this.open = false
-    },
-
-    getCaretPosition() {
-        return this.$refs.editor.selectionEnd
-    },
-
-    comboKeyUp(event) {
-        switch (event.key) {
-            case 'Enter':
-                this.choose(event)
-                break
-            case 'Escape':
+            choose(event) {
                 let caret = this.getCaretPosition()
-                this.$refs.editor.focus()
-                this.$refs.editor.setSelectionRange(caret, caret)
+                let before = this.content.substring(0, caret)
+                let after = this.content.substring(caret)
+
+                for (let strategy of this.strategy) {
+                    let pattern = strategy.pattern
+                    let extract = pattern.exec(before)
+                    if (null !== extract) {
+                        let newValue = strategy.replace(this.selected) + ' '
+                        caret += newValue.length - extract[0].length
+                        this.content = pattern[Symbol.replace](before, newValue) + after
+                        this.$nextTick(() => {
+                            this.$refs.editor.focus()
+                            this.$refs.editor.setSelectionRange(caret, caret)
+                        })
+                        break
+                    }
+                }
                 this.open = false
-                break
-        }
-    }
+            },
+
+            getCaretPosition() {
+                return this.$refs.editor.selectionEnd
+            },
+
+            comboKeyUp(event) {
+                switch (event.key) {
+                    case 'Enter':
+                        this.choose(event)
+                        break
+                    case 'Escape':
+                        let caret = this.getCaretPosition()
+                        this.$refs.editor.focus()
+                        this.$refs.editor.setSelectionRange(caret, caret)
+                        this.open = false
+                        break
+                }
+            }
+        })
 }
