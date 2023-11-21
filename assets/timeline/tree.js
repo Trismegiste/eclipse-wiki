04c5545617b72cc858c41ec103135896c93c8c6f
Trismegiste <flo@trismegiste.net>
2023-11-21 11:13:11 +0100

    ok with index

--- assets/timeline/tree.js
+++ assets/timeline/tree.js
@@ -13,10 +13,17 @@ export class TimelineTree {
     }
 
     recursiveFlat(level, node, listing, parent, childIdx) {
-        console.log(node)
         listing.push({level, title: node.data.title, finished: node.data.finished, parent, childIdx})
         for (const [idx, child] of node.nodes.entries()) {
             this.recursiveFlat(level + 1, child, listing, node, idx)
         }
+    }
+
+    moveNodeAfter(sourceIdx, targetIdx) {
+        let flatten = this.getFlatList()
+        let source = flatten[sourceIdx]
+        let target = flatten[targetIdx]
+        let removed = source.parent.nodes.splice(source.childIdx, 1)
+        target.parent.nodes.splice(target.childIdx, 0, removed[0])
     }
 }
