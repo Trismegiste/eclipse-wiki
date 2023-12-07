8deef0a838942d8e4026d1ce480feee31835d752
Trismegiste <flo@trismegiste.net>
2023-12-07 14:15:26 +0100

    adding cache

--- src/Service/InfoDashboard.php
+++ src/Service/InfoDashboard.php
@@ -7,13 +7,17 @@
 namespace App\Service;
 
 use App\Repository\VertexRepository;
+use DateInterval;
 use Symfony\Contracts\Cache\CacheInterface;
+use Symfony\Contracts\Cache\ItemInterface;
 
 /**
  * General information for the dashboard (cached)
  */
 class InfoDashboard
 {
+
+    const reasonableDelay = '1 hour';
 
     public function __construct(
             protected CacheInterface $cache,
@@ -26,22 +30,34 @@ class InfoDashboard
 
     public function getBrokenLinkCount(): int
     {
-        return count($this->digraph->searchForBrokenLink());
+        return $this->cache->get('dashboard_broken_link', function (ItemInterface $item) {
+                    $item->expiresAfter(DateInterval::createFromDateString(self::reasonableDelay));
+                    return count($this->digraph->searchForBrokenLink());
+                });
     }
 
     public function getBrokenPictureCount(): int
     {
-        return count($this->storage->searchForBrokenPicture($this->repository->search()));
+        return $this->cache->get('dashboard_broken_picture', function (ItemInterface $item) {
+                    $item->expiresAfter(DateInterval::createFromDateString(self::reasonableDelay));
+                    return count($this->storage->searchForBrokenPicture($this->repository->search()));
+                });
     }
 
     public function getOrphanCount(): int
     {
-        return count($this->digraph->findOrphan());
+        return $this->cache->get('dashboard_orphan', function (ItemInterface $item) {
+                    $item->expiresAfter(DateInterval::createFromDateString(self::reasonableDelay));
+                    return count($this->digraph->findOrphan());
+                });
     }
 
     public function getVertexCount(): int
     {
-        return array_sum(array_column(iterator_to_array($this->repository->countByClass()), 'total'));
+        return $this->cache->get('dashboard_vertex', function (ItemInterface $item) {
+                    $item->expiresAfter(DateInterval::createFromDateString(self::reasonableDelay));
+                    return array_sum(array_column(iterator_to_array($this->repository->countByClass()), 'total'));
+                });
     }
 
 }
--- tests/Service/InfoDashboardTest.php
+++ tests/Service/InfoDashboardTest.php
@@ -4,6 +4,7 @@
  * eclipse-wiki
  */
 
+use App\Repository\VertexRepository;
 use App\Service\InfoDashboard;
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
 
@@ -11,15 +12,42 @@ class InfoDashboardTest extends KernelTestCase
 {
 
     protected InfoDashboard $sut;
+    protected VertexRepository $repository;
 
     protected function setUp(): void
     {
         $this->sut = static::getContainer()->get(InfoDashboard::class);
+        $this->repository = static::getContainer()->get(VertexRepository::class);
+    }
+
+    public function testClean()
+    {
+        $this->repository->delete(iterator_to_array($this->repository->search()));
+        $this->assertCount(0, $this->repository->search());
     }
 
     public function testVertexCount()
     {
-        $this->sut->getVertexCount();
+        $vertex = new App\Entity\Scene('scene1');
+        $vertex->setContent('[[missing]] [[file:missing.jpg]]');
+        $this->repository->save($vertex);
+
+        $this->assertEquals(1, $this->sut->getVertexCount());
+    }
+
+    public function testBrokenLink()
+    {
+        $this->assertEquals(1, $this->sut->getBrokenLinkCount());
+    }
+
+    public function testBrokenPicture()
+    {
+        $this->assertEquals(1, $this->sut->getBrokenPictureCount());
+    }
+
+    public function testOrphan()
+    {
+        $this->assertEquals(1, $this->sut->getOrphanCount());
     }
 
 }
