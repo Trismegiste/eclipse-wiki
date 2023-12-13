81540cae6dbe53b95a9658c2ac7f32ac03c24d54
Trismegiste <flo@trismegiste.net>
2023-12-13 14:28:24 +0100

    new port

--- bin/launch.sh
+++ bin/launch.sh
@@ -2,7 +2,7 @@
 composer install
 bin/console cache:clear
 bin/console ass:com
-bin/console auth:get-link --qrcode
+bin/console auth:get-link 80 --qrcode
 bin/console p:d &
 cd /app/public
 frankenphp php-server
