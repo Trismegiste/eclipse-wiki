d7513f08fe08686299e091fa61d9b82fa77be20f
Trismegiste <flo@trismegiste.net>
2024-01-08 17:20:22 +0100

    clearer code

--- src/Form/Type/RandomIntegerType.php
+++ src/Form/Type/RandomIntegerType.php
@@ -8,6 +8,7 @@ namespace App\Form\Type;
 
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\OptionsResolver\OptionsResolver;
 
 /**
  * An IntegerType with a random button
@@ -18,6 +19,14 @@ class RandomIntegerType extends AbstractType
     public function getParent(): ?string
     {
         return IntegerType::class;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefault('attr', [
+            'class' => 'pure-input-1',
+            'x-model.fill' => 'result'
+        ]);
     }
 
 }
