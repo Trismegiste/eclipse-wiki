ccc4fe3a1396b9111cc18da6b1612f5268cdc2a5
Trismegiste <flo@trismegiste.net>
2024-02-23 15:30:34 +0100

    adding cypress to the project

---
+++ cypress.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    e2e: {
+        setupNodeEvents(on, config) {
+            // implement node event listeners here
+        },
+        baseUrl: 'http://192.168.69.177',
+        viewportWidth: 1280
+    },
+};
