7bcba087286ab966f83bc1709f921de9bbb891a2
Trismegiste <flo@trismegiste.net>
2024-03-04 23:50:51 +0100

    new way of doing collection with alpine

--- src/Form/PlaceAppendMorphBank.php
+++ src/Form/PlaceAppendMorphBank.php
@@ -7,6 +7,9 @@
 namespace App\Form;
 
 use App\Entity\Place;
+use App\Form\Type\MorphInventory;
+use App\Form\Type\ProviderChoiceType;
+use App\Repository\MorphProvider;
 use App\Repository\VertexRepository;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\DataMapperInterface;
@@ -17,12 +20,12 @@ use Symfony\Component\OptionsResolver\OptionsResolver;
 use Traversable;
 
 /**
- * Form for appending a morph bank inventory & price  to the content of a Place
+ * Form for appending a morph bank inventory & price to the content of a Place
  */
 class PlaceAppendMorphBank extends AbstractType implements DataMapperInterface
 {
 
-    public function __construct(protected VertexRepository $repository)
+    public function __construct(protected VertexRepository $repository, protected MorphProvider $morph)
     {
         
     }
@@ -30,7 +33,19 @@ class PlaceAppendMorphBank extends AbstractType implements DataMapperInterface
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-                ->add('inventory', CollectionType::class)
+                ->add('morph_list', ProviderChoiceType::class, [
+                    'provider' => $this->morph,
+                    'placeholder' => '--- Ajouter un Morphe ---',
+                    'attr' => [
+                        'x-on:change' => 'morphSelect'
+                    ]
+                ])
+                ->add('inventory', CollectionType::class, [
+                    'entry_type' => MorphInventory::class,
+                    'allow_add' => true,
+                    'attr' => ['x-ref' => 'inventory'],
+                    'block_prefix' => 'morph_inventory'
+                ])
                 ->add('append', SubmitType::class)
                 ->setMethod('PUT')
         ;
@@ -40,7 +55,8 @@ class PlaceAppendMorphBank extends AbstractType implements DataMapperInterface
     public function configureOptions(OptionsResolver $resolver): void
     {
         $resolver->setDefaults([
-            'data_class' => Place::class
+            'data_class' => Place::class,
+            'attr' => ['x-data' => 'morphBank']
         ]);
     }
 
---
+++ src/Form/Type/MorphInventory.php
@@ -0,0 +1,29 @@
+<?php
+
+/*
+ * Eclipse Wiki
+ */
+
+namespace App\Form\Type;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+
+/**
+ * Inventory of one Morph
+ */
+class MorphInventory extends AbstractType
+{
+
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+                ->add('morph', TextType::class)
+                ->add('stock', IntegerType::class)
+                ->add('dispo', IntegerType::class)
+        ;
+    }
+
+}
