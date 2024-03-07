2441d48a0ef31240dade566d2adb6144d9cf9fac
Trismegiste <flo@trismegiste.net>
2024-03-07 16:41:31 +0100

    e2e test for morph bank

---
+++ cypress/e2e/morph-bank.cy.js
@@ -0,0 +1,29 @@
+import { deleteVertex } from './business';
+
+describe('Morph bank creation', () => {
+
+    const vertexName = 'Resleeving center'
+
+    it('deletes Place if already existing', () => {
+        deleteVertex(vertexName)
+    })
+
+    it('creates the Place', () => {
+        cy.visit('/place/create')
+
+        cy.get('#place_title').type(vertexName)
+        cy.get('#place_content').type('Inventory')
+        cy.get('#place_create').click()
+    })
+
+    it('edits the place content', () => {
+        cy.visit('/wiki/' + vertexName)
+        cy.get('.big-title .icon-edit').click()
+
+        cy.get('main .minitoolbar .icon-sleeve').click()
+        cy.get('#place_append_morph_bank_morph_list').select('Huldre')
+        cy.get('#place_append_morph_bank_append').click()
+        cy.get('.parsed-wikitext table caption').should('contain', 'Banque de morphes')
+        cy.get('.parsed-wikitext td').should('contain', 'Huldre')
+    })
+})
