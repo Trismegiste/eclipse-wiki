0c33b89baafc3dc8f60e26fb4ee3d3897c357031
Trismegiste <flo@trismegiste.net>
2024-04-13 13:15:51 +0200

    POC

--- src/Parsoid/TagHandler/PushPublic.php
+++ src/Parsoid/TagHandler/PushPublic.php
@@ -6,10 +6,26 @@
 
 namespace App\Parsoid\TagHandler;
 
+use Wikimedia\Parsoid\Ext\ExtensionTagHandler;
+use Wikimedia\Parsoid\Ext\ParsoidExtensionAPI;
+
 /**
  * A section validated to be pushed on public channel (a.k.a "read aloud" section) <pushpublic>
  */
-class PushPublic extends \Wikimedia\Parsoid\Ext\ExtensionTagHandler
+class PushPublic extends ExtensionTagHandler
 {
-    
+
+    public function sourceToDom(ParsoidExtensionAPI $extApi, string $src, array $extArgs)
+    {
+        $doc = $extApi->getTopLevelDoc();
+        $node = $extApi->wikitextToDOM($src, ['parseOpts' => ['extTag' => 'carrousel', 'context' => 'inline']], false);
+        $container = $doc->createElement('section');
+        $container->setAttribute('class', 'read-aloud');
+        $container->appendChild($node);
+        $fragment = $doc->createDocumentFragment();
+        $fragment->appendChild($container);
+
+        return $fragment;
+    }
+
 }
