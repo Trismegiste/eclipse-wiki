39f1141b23f18882c805ea82ab75c66f10deb140
Trismegiste <flo@trismegiste.net>
2024-05-22 01:46:14 +0200

    investigating with dbus error in chromium

--- bin/launch.sh
+++ bin/launch.sh
@@ -5,6 +5,9 @@ mkdir /run/dbus
 dbus-daemon --system
 dbus-daemon --session --fork --print-address 1 > /tmp/socket-dbus.txt
 export DBUS_SESSION_BUS_ADDRESS=$(cat /tmp/socket-dbus.txt)
+# Need to replace previous line ?
+# export DBUS_SESSION_BUS_ADDRESS=`dbus-daemon --fork --config-file=/usr/share/dbus-1/session.conf --print-address`
+# /etc/init.d/dbus restart
 
 # Starting PHP app
 composer install
