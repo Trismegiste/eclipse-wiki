65cdb41ca190fa2be43a502ab37cf3136583d719
Trismegiste <flo@trismegiste.net>
2024-06-30 21:43:19 +0200

    dump into json

--- src/Command/LlmBench.php
+++ src/Command/LlmBench.php
@@ -2,10 +2,12 @@
 
 namespace App\Command;
 
+use App\Service\LlmIterator;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
 use Symfony\Contracts\HttpClient\HttpClientInterface;
 
 #[AsCommand(name: 'llm:bench')]
@@ -19,16 +21,22 @@ class LlmBench extends Command
 
     public function execute(InputInterface $input, OutputInterface $output): int
     {
+        $io = new SymfonyStyle($input, $output);
+        $bench = [];
         for ($k = 0; $k < 10; $k++) {
-            $iter = new \App\Service\LlmIterator($this->ollamaClient, 'eclipse-phase',
-                    "Fais un historique en 7 points d'un écumeur qui vit sur une barge. C'est un homme, un technicien, spécialisé dans la réparation de moteurs à fusion. Un des 7 points doit comporter un evenement tragique et un autre point doit concerner sa famille");
+            $io->title("Historique $k");
+            $answer = '';
+            $iter = new LlmIterator($this->ollamaClient, 'eclipse-phase',
+                    "Fais un historique sur 7 points d'un écumeur qui vit sur une barge. C'est un homme, un technicien, spécialisé dans la réparation de moteurs à fusion. Un des 7 points doit comporter un evenement tragique et un autre point doit concerner sa famille");
 
             foreach ($iter as $val) {
-                $output->write($val);
+                $answer .= $val;
             }
-            $output->writeln('-------------------------');
-            $output->writeln('');
+            $bench[] = $answer;
+            $io->writeln($answer);
         }
+
+        file_put_contents('bench.json', json_encode($bench));
 
         return self::SUCCESS;
     }
