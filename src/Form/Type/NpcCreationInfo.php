4d8c5c5ab53b5c071ce977057929cf440fa12aa5
Trismegiste <flo@trismegiste.net>
2024-10-31 15:42:03 +0100

    adding 3 Place at NPC creation

--- src/Form/Type/NpcCreationInfo.php
+++ src/Form/Type/NpcCreationInfo.php
@@ -6,15 +6,24 @@
 
 namespace App\Form\Type;
 
+use App\Entity\Place;
 use App\Form\Type\WikitextType;
+use App\Repository\VertexRepository;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\DataMapperInterface;
 use Symfony\Component\Form\FormBuilderInterface;
+use Traversable;
 
 /**
  * Quick info fields for a NPC that compile into the content
  */
-class NpcCreationInfo extends AbstractType implements \Symfony\Component\Form\DataMapperInterface
+class NpcCreationInfo extends AbstractType implements DataMapperInterface
 {
+
+    public function __construct(protected VertexRepository $repository)
+    {
+        
+    }
 
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
@@ -27,23 +36,55 @@ class NpcCreationInfo extends AbstractType implements \Symfony\Component\Form\Da
                         'rows' => 2
                     ]
                 ])
+                ->add('habitat', AutocompleteType::class, [
+                    'choices' => $this->getPlaceTitle(),
+                    'required' => false
+                ])
+                ->add('work', AutocompleteType::class, [
+                    'choices' => $this->getPlaceTitle(),
+                    'required' => false
+                ])
+                ->add('fun', AutocompleteType::class, [
+                    'choices' => $this->getPlaceTitle(),
+                    'required' => false
+                ])
                 ->setDataMapper($this)
         ;
     }
 
-    public function mapDataToForms(mixed $viewData, \Traversable $forms): void
+    public function mapDataToForms(mixed $viewData, Traversable $forms): void
     {
         
     }
 
-    public function mapFormsToData(\Traversable $forms, &$viewData): void
+    public function mapFormsToData(Traversable $forms, &$viewData): void
     {
         if (is_null($viewData)) {
             return;
         }
 
         $field = iterator_to_array($forms);
-        $viewData = $field['info']->getData();
+        $viewData = $field['info']->getData()
+                . "\n"
+                . $this->getFilledTemplate($field['work']->getData(),
+                        $field['fun']->getData(),
+                        $field['habitat']->getData());
+    }
+
+    protected function getFilledTemplate(?string $work, ?string $fun, ?string $habitat): string
+    {
+        return "{{location\n|habitat=$habitat\n|work=$work\n|fun=$fun\n}}";
+    }
+
+    protected function getPlaceTitle(): iterable
+    {
+        // @TODO add a method searchOnlyTitleByClass. Also usable in SceneContentWizardType
+        $choice = [];
+        foreach ($this->repository->findByClass(Place::class) as $vertex) {
+            $choice[] = $vertex->getTitle();
+        }
+
+        return $choice;
     }
 
 }
