b8cc7b7d8de0268733e27fe3c61a243705f26626
Trismegiste <flo@trismegiste.net>
2024-11-08 17:17:05 +0100

    mor readable tests

--- tests/Controller/OllamaTest.php
+++ tests/Controller/OllamaTest.php
@@ -23,10 +23,16 @@ class OllamaTest extends WebTestCase
         $this->repository = static::getContainer()->get(VertexRepository::class);
     }
 
-    public function testNpcBackground()
+    public function testClean()
+    {
+        $this->repository->delete(iterator_to_array($this->repository->search()));
+        $this->assertCount(0, iterator_to_array($this->repository->search()));
+    }
+
+    public function testBackgroundButton()
     {
         $factory = static::getContainer()->get(CharacterFactory::class);
-        $npc = $factory->create('takeshi' . rand(), new Background('bg'), new Faction('diplo'));
+        $npc = $factory->create('takeshi kovacs', new Background('bg'), new Faction('diplo'));
         $npc->setContent('information');
         $npc->setMorph(new Morph('dummy'));
         $this->repository->save($npc);
@@ -41,22 +47,30 @@ class OllamaTest extends WebTestCase
                 ->filter('i[class=icon-background]'); // just to be sure we select the background icon inside the minitoolbar inside the edit form
         $this->assertCount(1, $button);
 
-        $crawler = $this->client->click($button->ancestors()->link());
+        return $button->ancestors()->link()->getUri();
+    }
+
+    /** @depends testBackgroundButton */
+    public function testBackgroundGeneration(string $uri)
+    {
+        $crawler = $this->client->request('GET', $uri);
         $this->assertResponseIsSuccessful();
         $this->assertSelectorExists('#npc_background_generate');
-        $this->assertSelectorNotExists('#llm_output_append_save');
-        $this->assertFormValue('form[name=npc_background]', 'npc_background[title]', $npc->getTitle());
+        $this->assertSelectorNotExists('#llm_output_append_save');  // the form to append the generated content is not here yet
+        $this->assertFormValue('form[name=npc_background]', 'npc_background[title]', 'Takeshi');
         $form = $crawler->selectButton('npc_background_generate')->form();
 
+        // post the parameters for the prompt
         $form->setValues(['npc_background' => ['role' => 'diplo']]);
         $crawler = $this->client->submit($form);
         $this->assertResponseIsSuccessful();
 
+        // the form to append the generated content is here now
         $this->assertSelectorExists('#llm_output_append_save');
         $form = $crawler->selectButton('llm_output_append_save')->form();
         $this->assertStringStartsNotWith($form->getValues()['llm_output_append[prompt_query]'], 'Dans le contexte');
 
-        $form->setValues(['llm_output_append' => ['generation' => 'Harlan']]);
+        $form->setValues(['llm_output_append' => ['generation' => 'Harlan']]); // fill the content (inside a real browser, it is filled by a javascript)
         $crawler = $this->client->submit($form);
         $this->assertResponseRedirects();
         $this->client->followRedirect();
